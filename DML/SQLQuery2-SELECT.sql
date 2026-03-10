c4522b6e33e80e21cacb7b84268ac07769ced7ee
irina <irina@COMPUTER>
2026-03-10 21:05:00 +0500

    Backup

---
+++ DML/SQLQuery2-SELECT.sql
@@ -0,0 +1,12 @@
+--SQLQuery2-SELECT.sql
+USE PV_522_Import;
+
+--ALTER TABLE		Groups
+--ALTER COLUMN	start_time TIME(0) NOT NULL;
+
+SELECT
+		group_name		AS	N'Группа',
+		direction_name	AS	N'Направаление обучения'
+FROM	Groups,Directions
+WHERE	direction=direction_id
+;
